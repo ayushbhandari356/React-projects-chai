06be76bb601502de60e86b81c29b9b74482601fc
ayushbhandari356 <bhandaria351@gmail.com>
2024-03-16 23:24:59 +0530

    bug resolved in env file

---
+++ 12megaBlogWebsite/src/components/container/Container.js
@@ -0,0 +1,11 @@
+import React from 'react'
+// children is just a variable taking property values
+function Container({children}) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Container
